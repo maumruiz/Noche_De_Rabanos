b4ad7eb1d8cde75ae632779a08267c85516c24c5
maumruiz <mau.mruiz@gmail.com>
2023-12-23 04:46:21 -0600

    Update splat

--- src/components/OurArtwork.jsx
+++ src/components/OurArtwork.jsx
@@ -18,10 +18,10 @@ export default function OurArtwork() {
 
   return (
     <>
-      <Canvas dpr={1.5} gl={{ antialias: false }} camera={{ position: [0, 0, 2] }}>
+      <Canvas dpr={1.5} gl={{ antialias: false }} camera={{ position: [0, 0, 3] }}>
         <CameraControls makeDefault />
         {/* <StatsGl /> */}
-        <Splat alphaTest={0.1} src={`/cleaned.splat`} scale={0.5} position={[0, 0, 0]} />
+        <Splat alphaTest={0.1} src={`/manos.splat`} scale={0.5} position={[0, 0, 0]} />
       </Canvas>
 
       {/* <div style={{visibility: started ? 'hidden' : 'visible' }} */}
