8653152086d2fa695be7c49ab810388957bd5135
maumruiz <mau.mruiz@gmail.com>
2023-12-23 01:51:34 -0600

    Add gaussian splatting

--- src/components/OurArtwork.jsx
+++ src/components/OurArtwork.jsx
@@ -1,10 +1,16 @@
+import { Canvas } from '@react-three/fiber'
+import { Splat, Float, CameraControls, StatsGl, Effects } from '@react-three/drei'
 import Navbar from './Navbar'
 
 export default function OurArtwork() {
   return (
     <>
+      <Canvas dpr={1.5} gl={{ antialias: false }} camera={{ position: [0, 0, 2] }}>
+        <CameraControls makeDefault />
+        {/* <StatsGl /> */}
+        <Splat alphaTest={0.1} src={`/cleaned.splat`} scale={0.5} position={[0, 0, 0]} />
+      </Canvas>
       <Navbar />
-      <div style={{ color: "white", marginTop: "200px" }}>Our Artwork</div>
     </>
   )
 }
