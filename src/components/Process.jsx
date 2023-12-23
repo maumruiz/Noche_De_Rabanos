056113fecf4ff092f81f71195cda90bf792fc914
maumruiz <mau.mruiz@gmail.com>
2023-12-23 03:31:45 -0600

    Process page

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,10 +1,17 @@
 import Navbar from './Navbar'
+import styles from './Process.module.css'
 
 export default function Process() {
   return (
     <>
+      <div className={styles.container}>
+        <div className={styles.toast}>
+          Página en construcción
+        </div>
+        <p> Estaremos publicando videos de nuestro proceso para crear la obra "manos artesanas" </p>
+        {/* <p> Por lo pronto, dejamos aqui algunos timelapses </p> */}
+      </div>
       <Navbar />
-      <div style={{ color: "white", marginTop: "200px" }}>Our Process</div>
     </>
   )
 }
