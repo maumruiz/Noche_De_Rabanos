50247da9c61ca5a3888c14aa0d7b8e1119f5b907
maumruiz <mau.mruiz@gmail.com>
2023-12-23 01:31:04 -0600

    fix typos

--- src/store.js
+++ src/store.js
@@ -14,8 +14,8 @@ export const dialogos = [
     "Estas raíces crecieron durante meses apuntando hacia el centro de la Tierra.",
     "Como rayos de Luna fueron penetrando el suelo hasta que una noche, la creatividad humana las hace girar hacia el centro de la galaxia.",
     "Las manos de los artesanos del rábano sienten el frío del agua en su blancura, la fuerza de la Tierra en su piel enrojecida y la ternura del mundo en el verdor en sus hojas.",
-    "Las manos son el enlace perfecto entre huesos, ligamentos, musculos y nervios, forma y función, movimiento y sensación.",
-    "Cada obra que se muestra esta noche surge de la danza dactilar del ArteSano que abre su corazón como un homenaje a la Vida-Muerte-Vida.",
-    "Son incontables las manos de quienes durante más de 125 años han enaltecido las enigmáticas formas de está raíz.",
+    "Las manos son el enlace perfecto entre huesos, ligamentos, músculos y nervios, forma y función, movimiento y sensación.",
+    "Cada obra que se muestra esta noche surge de la danza dactilar del Artesano que abre su corazón como un homenaje a la Vida-Muerte-Vida.",
+    "Son incontables las manos de quienes durante más de 125 años han enaltecido las enigmáticas formas de esta raíz.",
     "Hoy reaparecen como símbolo viviente de las raíces que nutren a un pueblo en esplendor permanente.",
 ]
